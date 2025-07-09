7c80f88559bea9357faf3dedfbc13d435b4da12e
Velizara Sotirova <114175948+VelizaraSotirova@users.noreply.github.com>
2025-07-09 15:41:36 +0300

    Copied files
    Copied _ViewStart, _ViewImports, _ValidationScriptsPartial to reuse the project layout. Now the UI is much better

--- GrandHotelPetrichMVC.Web/obj/Debug/net8.0/GrandHotelPetrichMVC.Web.AssemblyInfo.cs
+++ GrandHotelPetrichMVC.Web/obj/Debug/net8.0/GrandHotelPetrichMVC.Web.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrandHotelPetrichMVC.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf1303b71fd7ab7631cff4cfc0292a75bf86d86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7efc2b32153198815c6bdd91200b6aaaf26ae78d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrandHotelPetrichMVC.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrandHotelPetrichMVC.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
